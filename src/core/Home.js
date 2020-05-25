b820c82217250df678017304424ff39d895ba4b4
Aniket Ghosh <ghoshaniketkumar7@gmail.com>
2020-05-25 18:34:57 +0530

    signup is done

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import '../styles.css';
-
-
+import { API } from '../backEnd';
+import Base from './Base';
 
 export default function Home() {
+    console.log("API IS", API);
+
     return (
-        <div>
-            <h1 className="text-white">Hello front end</h1>
-        </div>
+        <Base title="Home Page" description="Welcome to the Tshirt Store">
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn btn-success">Test</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">Test</button>
+
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">Test</button>
+                </div>
+            </div>
+        </Base>
     );
 }
---
+++ src/user/Signup.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import Base from '../core/Base';
+import { Link } from 'react-router-dom';
+import { signup } from '../auth/helper/index';
+
+const Signup = () => {
+
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false
+    });
+
+    const { name, email, password, success } = values; // object destructured
+
+    // check higherOrder functions
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    };
+
+    const onSubmit = event => {
+        event.preventDefault();
+        setValues({ ...values, error: false });
+        signup({ name, email, password }) // this methods automatically sends an request to the server and returns a server side response, that we need to deal accordingly
+            .then(data => {
+                if (data.error) {
+                    setValues({ ...values, error: data.error, success: false });
+                }
+                else {
+                    setValues({
+                        ...values,
+                        name: "",
+                        email: "",
+                        password: "",
+                        error: "",
+                        success: true
+                    }); //setting the state once again
+                    // console.log(`Data that has come back: `+ JSON.stringify(data));
+                }
+            })
+            .catch(err => {
+                console.log("Error is: " + err);
+            });
+    };
+
+    const signUpForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form action="">
+                        <div className="form-group">
+                            <label className="text-light">Name</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                onChange={handleChange("name")}
+                                value={name}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" onChange={handleChange("email")} type="email"
+                                value={email}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control"
+                                value={password}
+                                onChange={handleChange("password")} type="password" />
+                        </div>
+                        <div className="btn btn-success btn-block" onClick={onSubmit}>Submit</div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+    const successMesssage = () => {
+        return (
+            <div className="alert alert-sucess"
+                style={{ display: success ? "" : "none" }}
+            >
+                New Account was created sucessfully. Please <Link to="/signin">Login Here</Link>
+            </div>
+        );
+    };
+
+    const errorMesssage = () => {
+        return (
+            <div className="alert alert-danger"
+                style={{ display: success ? "" : "none" }}
+            >
+                error
+            </div>
+        );
+    };
+
+    return (
+        <Base title="SignUp Page" description="A page for a user to SignUp!">
+            {successMesssage()}
+            {errorMesssage()}
+            {signUpForm()}
+            <p className="text-white text-center">
+                {JSON.stringify(values)}
+            </p>
+        </Base>
+    );
+};
+
+export default Signup;
